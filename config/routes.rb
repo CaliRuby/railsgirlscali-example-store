17fe0fc83594e5729419e356112f8cb50099cf4c
Victor Cortes <ie.cortex@gmail.com>
2016-04-24 17:44:29 -0500

    Add namespace admin to create, update and destroy products

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   get 'products/:id/purchase', to: 'products#purchase', as: :purchase_product
 
+  namespace :admin do
+    resources :products
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
